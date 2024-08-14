918f926c819ddcfe82ae0fbe6bd59658b7d536a4
Volodymyr Zhylyayev <vzhilyaev@gmail.com>
2024-08-14 22:15:06 +0300

    refactor: Use ruby  Logger class for logging

--- app.rb
+++ app.rb
@@ -12,6 +12,13 @@ require_relative 'lib/telegram_api'
 
 IMAGE_SET = %i[buy_sell_graph ratio_graph diff_graph].freeze
 
+def images
+  GraphGenerator.new(rates: CurrencyRate.historical_rates).then do |g|
+    IMAGE_SET.map { |name| g.public_send(name) }
+  end
+end
+
+def message = MessageGenerator.new(rates: @fetched_rates).message
 def test_run? = ENV['TEST_RUN'] == 'yes'
 
 def same_rates?
@@ -23,25 +30,16 @@ def same_rates?
   previous_rates.sell == @fetched_rates.sell && previous_rates.buy == @fetched_rates.buy
 end
 
-def log_record(message) = puts("#{Time.now} #{message}")
-
-def message = MessageGenerator.new(rates: @fetched_rates).message
-
-def images
-  GraphGenerator.new(rates: CurrencyRate.historical_rates).then do |g|
-    IMAGE_SET.map { |name| g.public_send(name) }
-  end
-end
-
 # Notify and store rates
 begin
+  logger = Logger.new($stdout)
   @fetched_rates = CurrencyRate.build(MonoApi.fetch_rates(test_run: test_run?))
   return if !test_run? && same_rates?
 
   @fetched_rates.save! unless test_run?
-  log_record @fetched_rates.attributes.to_s
+  logger.info(@fetched_rates.attributes.to_s)
   CurrencyRate.perform_housekeeping
   TelegramApi.send_message(images:, message:)
 rescue StandardError => e
-  log_record "#{e.class} - #{e.message}"
+  logger.error("#{e.class} - #{e.message}")
 end
