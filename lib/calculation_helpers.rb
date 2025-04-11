c9ae69e82de51c396fca46b1cbb162b0fa49d330
Volodymyr Zhylyayev <vzhilyaev@gmail.com>
2025-04-11 21:31:27 +0300

    fix: No data case

--- lib/calculation_helpers.rb
+++ lib/calculation_helpers.rb
@@ -11,5 +11,5 @@ module CalculationHelpers
   def conversion_diff(rates) = (NBU_LIMIT * ((1.0 / rates.buy) - (1.0 / rates.sell))).round(2)
 
   def min_diff_id(rates) = rates.min_by { |rate| rate.sell - rate.buy }.id
-  def min_rate_in_increments(rates, increment) = (rates.min_by(&:buy).buy / increment).floor * increment
+  def min_rate_in_increments(rates, increment) = ((rates.min_by(&:buy)&.buy || 0) / increment).floor * increment
 end
