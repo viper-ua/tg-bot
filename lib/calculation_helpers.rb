31da3c704ed63319d46e510ab0af6c80a6d95b51
Volodymyr Zhylyayev <vzhilyaev@gmail.com>
2025-04-22 17:39:34 +0300

    chore: Better impementation of #min_diff_id helper method

--- lib/calculation_helpers.rb
+++ lib/calculation_helpers.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 # Various calculations for rates analysis
 module CalculationHelpers
   NBU_LIMIT = 50_000.0
@@ -10,6 +12,19 @@ module CalculationHelpers
   def sell_to_limit(rate) = (NBU_LIMIT / rate.buy).round(2)
   def conversion_diff(rate) = (NBU_LIMIT * ((1.0 / rate.buy) - (1.0 / rate.sell))).round(2)
 
-  def min_diff_id(rates) = rates.reverse.min_by { |rate| rate.sell - rate.buy }&.id
+  def min_diff_id(rates)
+    return nil unless rates.is_a?(Array) && rates.size > 0
+
+    min_id = nil
+    min_val = Float::INFINITY
+    rates.reverse_each do |rate|
+      next unless rate.sell - rate.buy < min_val
+
+      min_id = rate.id
+      min_val = rate.sell - rate.buy
+    end
+    min_id
+  end
+
   def min_rate_in_increments(rates, increment) = ((rates.min_by(&:buy)&.buy || 0) / increment).floor * increment
 end
