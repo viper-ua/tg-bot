86315712d220f55db05e877d89a473263fee997b
Volodymyr Zhylyayev <vzhilyaev@gmail.com>
2025-05-29 00:39:07 +0300

    refactor: Improve balance message generation in BalanceMessageGenerator with enhanced formatting and separation of concerns

--- lib/generators/balance_message_generator.rb
+++ lib/generators/balance_message_generator.rb
@@ -2,28 +2,40 @@
 
 # Class responsible for generating balance report messages
 class BalanceMessageGenerator
-  class << self
-    def message(balances:)
-      <<~MESSAGE
-        <b><i>#{Time.now}</i></b>
-        #{balances.map { |acc| format_account(acc) }.join}
-      MESSAGE
-    end
+  def message(balances:)
+    <<~MESSAGE
+      #{header}
+      #{format_balances(balances)}
+    MESSAGE
+  end
 
-    private
+  private
 
-    def format_account(account)
-      balance = format_amount(account['balance'] - account['creditLimit'])
-      credit_limit = format_amount(account['creditLimit']) if account['creditLimit'].positive?
+  def header
+    "<b><i>#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}</i></b>"
+  end
 
-      <<~ACCOUNT
-        <b>#{account['type']}}:</b> #{balance}#{credit_limit ? " (#{credit_limit})" : ''}
-      ACCOUNT
-    end
+  def format_balances(balances)
+    balances.map { |account| format_account(account) }.join
+  end
 
-    def format_amount(amount)
-      amount = amount.to_f / 100 # Convert from cents to main currency
-      format('%.2f', amount)
-    end
+  def format_account(account)
+    balance = format_amount(account['balance'] - account['creditLimit'])
+    credit_limit = format_credit_limit(account['creditLimit'])
+
+    <<~ACCOUNT
+      <b>#{account['type']}:</b> #{balance}#{credit_limit}
+    ACCOUNT
+  end
+
+  def format_credit_limit(credit_limit)
+    return '' unless credit_limit.positive?
+
+    " (#{format_amount(credit_limit)})"
+  end
+
+  def format_amount(amount)
+    amount = amount.to_f / 100 # Convert from cents to main currency
+    format('%.2f', amount)
   end
 end
